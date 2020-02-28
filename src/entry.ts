da3425975eb5c515b2de2374388f3d961242d13b
Vladimir <Kozhaev>
2020-02-28 12:49:47 +0200

    Save view of the nodes

--- src/entry.ts
+++ src/entry.ts
@@ -14,24 +14,59 @@ function delay() {
 
     var myDiagram = $( go.Diagram, "myDiagramDiv",  // create a Diagram for the DIV HTML element
         {
-        layout: $(go.TreeLayout,
-                { angle: 0, nodeSpacing: 10, layerSpacing: 30 }),
+            layout: $( go.TreeLayout,
+                { angle: 0, nodeSpacing: 10, layerSpacing: 30 } ),
             "undoManager.isEnabled": true  // enable undo & redo
         } );
 
     // define a simple Node template
     myDiagram.nodeTemplate =
         $( go.Node, "Auto",  // the Shape will go around the TextBlock
-               
+
             $( go.Shape, "RoundedRectangle", { strokeWidth: 0, fill: "white" },
 
                 // Shape.fill is bound to Node.data.color
                 new go.Binding( "fill", "color" ) ),
-            $( go.TextBlock,
-                { margin: 8, font: "bold 14px sans-serif", stroke: '#333' }, // Specify a margin to add some room around the text
-                // TextBlock.text is bound to Node.data.key
-                new go.Binding( "text", "key" ) ),
-                
+            $( go.Panel, "Table",
+                $( go.RowColumnDefinition,
+                    { column: 0, alignment: go.Spot.Left } ),
+                $( go.RowColumnDefinition,
+                    { column: 1, alignment: go.Spot.Right } ),
+                $( go.TextBlock,  // the node title
+                    {
+                        column: 0, row: 0, columnSpan: 3, alignment: go.Spot.Center,
+                        font: "bold 10pt sans-serif", margin: new go.Margin( 4, 2 )
+                    },
+                    new go.Binding( "text", "key" ) ),
+                $( go.Panel, "Horizontal",
+                    { column: 0, row: 1 },
+                    $( go.Shape,  // the "A" port
+                        {
+                            width: 6, height: 6, portId: "A", toSpot: go.Spot.Right,
+                            fromLinkable: true, toMaxLinks: 1,toLinkable: true,
+                        } ),  // allow user-drawn links from here
+                    $( go.TextBlock, "In" )  // "A" port label
+                ),
+//                $( go.Panel, "Horizontal",
+//                    { column: 1, row: 1 },
+//                    $( go.Shape,  // the "B" port
+//                        {
+//                            width: 6, height: 6, portId: "B", toSpot: go.Spot.Left,
+//                            toLinkable: true, toMaxLinks: 1
+//                        } ),  // allow user-drawn links from here
+//                    $( go.TextBlock, "B" )  // "B" port label
+//                ),
+//                $( go.Panel, "Horizontal",
+//                    { column: 2, row: 1, rowSpan: 2 },
+//                    $( go.TextBlock, "Out" ),  // "Out" port label
+//                    $( go.Shape,  // the "Out" port
+//                        {
+//                            width: 6, height: 6, portId: "Out", fromSpot: go.Spot.Right,
+//                            fromLinkable: true
+//                        } )  // allow user-drawn links to here
+//                )
+            )
+
 
         );
 
@@ -41,34 +76,34 @@ function delay() {
             $( go.Shape,   // the arrowhead
                 { toArrow: "OpenTriangle", fill: null } ),
             {
-              
-                click:(e,link)=>{
-                  var l:go.Link = link as go.Link;
-                  var from=l.fromNode;
-                  var to=l.toNode;
-                  var nodeName:string=""+nodeDataArray.length
-                 
-                  myDiagram.model.addNodeData({key:""+nodeName,color:"lightblue"})
-                 
-                  linkModel.addLinkData({from:from.key as string,to:nodeName});
-                  linkModel.addLinkData({from:nodeName as string,to:to.key});
-                  myDiagram.remove(l)
+
+                click: ( e, link ) => {
+                    var l: go.Link = link as go.Link;
+                    var from = l.fromNode;
+                    var to = l.toNode;
+                    var nodeName: string = "" + nodeDataArray.length
+
+                    myDiagram.model.addNodeData( { key: "" + nodeName, color: "lightblue" } )
+
+                    linkModel.addLinkData( { from: from.key as string, to: nodeName } );
+                    linkModel.addLinkData( { from: nodeName as string, to: to.key } );
+                    myDiagram.remove( l )
                 }
             }
         );
 
     // but use the default Link template, by not setting Diagram.linkTemplate
-    var nodeDataArray=[{ key: "Alpha", color: "lightblue" },
+    var nodeDataArray = [{ key: "Alpha", color: "lightblue" },
     { key: "Beta", color: "orange" }]
-    var nodeLinkArray=[
-                       { from: "Alpha", to: "Beta" }
-                       
-                   ] ;
+    var nodeLinkArray = [
+        { from: "Alpha", to: "Beta" }
+
+    ];
     // create the model data that will be represented by Nodes and Links
-    
-    var linkModel:go.GraphLinksModel=new go.GraphLinksModel(
-            nodeDataArray,
-            nodeLinkArray );
+
+    var linkModel: go.GraphLinksModel = new go.GraphLinksModel(
+        nodeDataArray,
+        nodeLinkArray );
     myDiagram.model = linkModel;
 
 }
